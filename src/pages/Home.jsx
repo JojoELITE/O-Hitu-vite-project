1c5bf0a352f0b1fb45292efc1c33608749d31495
JojoELITE <jojoelite1014@gmail.com>
2024-04-02 16:47:16 +0100

    la section hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,28 +5,35 @@ import { Button } from "@/components/ui/button"
 function Home() {
 
   return (
-    < div className="py-10 px-24">
-        <nav className="flex justify-between">
-            <div className="flex gap-10">
+    < div className="py-12 px-28 bg-[#F2FAFC]">
 
-                <img src="./assets/Group_9__5_-removebg-preview 1.png" alt="logo"/>
+        <nav className="flex justify-between items-center">
 
-                <div>
+            <div className="flex gap-14 items-center">
 
-                    <Button className="text-[16px]" variant="online">Rechercher une structure sanitaire</Button>
+                <img src="assets/Group_9__5_-removebg-preview 1.png" alt="logo"/>
+
+                <div className="">
+                    <Button className="text-[16px] gap-4" variant="online">
+                        <img className="h-[66px] mt-8" src="assets/Search.png" alt="logo"/>
+                        <span className="mt-8">Rechercher une structure sanitaire</span>
+                    </Button>
                 </div>
 
             </div>
 
 
-            <div>
-                <Button variant="">LAISSER UN AVIS</Button>
+            <div className=""> 
+                <Button className="gap-4 w-[189px] h-[50px]" variant="">            
+                    <img src="assets/ph_star-fill.png" alt="logo"/>
+                    <span className="text-[12px]"> LAISSER UN AVIS</span>
+                </Button>
             </div>
 
         </nav>
 
 
-        <div className="py-10">
+        <div className="py-24 flex justify-between">
 
             <div>
 
@@ -43,26 +50,49 @@ function Home() {
                     </h6>
                 </div>
 
+                <div className="flex items-center gap-8">
 
-                <div className="py-10">
-                    <Button className=" w-[426.04px] h-[65.42px] border-2 text-[12px]" variant="online">Rechercher une structure</Button>
-                    <Button className=" w-[222.28px] h-[65.42px] border-2 text-[12px]" variant="online">Localisation</Button>
+                    <div className="py-10 flex">
+                        <Button className=" w-[426.04px] h-[80px] border-2 text-[16px] gap-4" variant="online">
+                            <img className="h-[66px]" src="assets/material-symbols-light_search.png" alt="logo"/>
+                            <span className="mr-32 mb-4">Rechercher une structure</span>
+                        </Button>
+                        
+                        <Button className=" w-[222.28px] h-[80px] border-2 text-[16px]" variant="online">
+                            <span className="mr-14 mb-4">Localisation</span>
+                            <img className="h-[44px]" src="assets/fluent_location-20-regular.png" alt="logo"/>
+                        </Button>
+                    </div>
+
+                        <img className="h-[90px] mt-4" src="assets/Search.png" alt="logo"/>
+
                 </div>
 
 
                 <div className="flex gap-4 ">
-                    <Button className="w-[141.25px]" variant="">Hopital</Button>
-                    <Button className="w-[141.25px]" variant="">Clinique</Button>
-                    <Button className="w-[141.25px]" variant="">Pharmacie</Button>
-                    <Button className="w-[141.25px]" variant="">PMI</Button>
+                    <Button className="w-[141.25px] h-[48px] bg-[#1A2E35]" variant="">Hopital</Button>
+                    <Button className="w-[141.25px] h-[48px] bg-[#1A2E35]" variant="">Clinique</Button>
+                    <Button className="w-[141.25px] h-[48px] bg-[#1A2E35]" variant="">Pharmacie</Button>
+                    <Button className="w-[141.25px] h-[48px] bg-[#1A2E35]" variant="">PMI</Button>
                 </div>
 
 
             </div>
 
+            <div>
 
+                <div className="w-[520px] h-[500px] bg-white"> 
+                    <h2 className="text-[#4B4B61] text-center py-10 text-[24px]">Filtrez: Les mieux notés</h2>
 
-            <div></div>
+                </div>
+
+                <a className="" href="#">
+                    <span className="text-[#DB4C4C] px-44">Afficher plus de structure </span>
+                </a>
+
+            </div>
+
+         
 
         </div>
 
