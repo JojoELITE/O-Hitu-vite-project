826ea1aa0ac4dc18253ffe28271ed199eb70c2e8
JojoELITE <jojoelite1014@gmail.com>
2024-04-03 15:39:50 +0100

    fin de la version mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,8 +53,8 @@ function Home() {
 
                 <div className="py-2 text-[8px]">
                     <h6 >
-                        Et si vous aviez le pouvoir de choisir le meilleur établissement de santé pour votre santé ? <br/>
-                            <span className="font-bold">O’Hitu</span> vous aide à faire votre choix en rassemblant les avis des patients et la note général<br/>
+                        Et si vous aviez le pouvoir de choisir le meilleur établissement de santé pour votre santé ?
+                            <span className="font-bold">O’Hitu</span> vous aide à faire votre choix en rassemblant les avis des patients et la note général
                         de Connexions 4.0.  
                     </h6>
                 </div>
