2f91731f91ff60765180a5ea0539e201efc44e18
JojoELITE <jojoelite1014@gmail.com>
2024-04-12 17:20:13 +0100

    Je travaille sur le responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -36,7 +36,7 @@ function NavBar() {
                         <DropdownMenuTrigger>
 
                             <Button className="text-[16px] gap-4 flex" variant="online">
-                                <img className="h-[40px] lg:h-[66px] ml-10 lg:ml-0" src="assets/Search.png" alt="logo" />
+                                <img className="h-[45px] lg:h-[66px] mb-8 lg:mb-0 ml-8 lg:ml-0" src="assets/Search.png" alt="logo" />
 
                                 <div>
                                     <span className="mb-4 hidden lg:block text-[#BDBDBD]">Rechercher une structure sanitaire</span>
