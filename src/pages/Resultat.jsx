b4c71713dec8f5ca7bff35e3364793926abb11cc
JojoELITE <jojoelite1014@gmail.com>
2024-04-16 10:03:48 +0100

    responsive de la page 2

--- src/pages/Resultat.jsx
+++ src/pages/Resultat.jsx
@@ -44,13 +44,13 @@ function Resultat() {
 
           </div>
 
-          <div className="flex justify-between items-center gap-24 lg:gap-2 text-[#BDBDBD] mt-4 lg:mt-0">
-            <form class="flex items-center">
+          <div className="gap-24 lg:gap-2 text-[#BDBDBD] mt-4 lg:mt-0 flex justify-between items-center">
+            <form class="lg:flex items-center hidden">
               <label for="" class="block px-3 lg:mt-2 text-[18px] text-[#4B4B61] dark:text-white ">Filtrez :</label>
 
 
               <Select className="" >
-                <SelectTrigger className="w-[50px] lg:w-[180px]">
+                <SelectTrigger className="w-[100px] lg:w-[180px]">
                   <SelectValue placeholder="" />
                 </SelectTrigger>
                 <SelectContent className="text-[#4B4B61] rounded-lg bg-white ">
@@ -59,7 +59,20 @@ function Resultat() {
                 </SelectContent>
               </Select>
 
+
             </form>
+
+
+
+            <form class="flex items-center gap-2 block lg:hidden">
+              <label for="notes" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filtrez :</label>
+              <select id="notes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w- p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected></option>
+                <option value="mieux">Les mieux notés</option>
+                <option value="moins">les moins notés</option>
+              </select>
+            </form>
+
 
             <Button className=" w-[130px] lg:w-[150px] h-[48px]  flex gap-2 rounded-full border-2 hover:border-gray-400" variant="online">
               <img className="sm:h-18" src="assets/carte.png" alt="logo" />
